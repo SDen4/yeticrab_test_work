06964b32b8947a620dd1de4a0abdec7223b355bd
SDen4 <asw325@yandex.ru>
2020-08-31 20:11:05 +0500

    initial commit

---
+++ build/webpack.base.config.js
@@ -0,0 +1,119 @@
+const path = require("path");
+const fs = require("fs");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    assets: "assets/"
+}
+
+const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+const PAGES = fs.readdirSync(PAGES_DIR).filter(filename => filename.endsWith('.pug'));
+
+
+module.exports = {
+
+    externals: {
+        paths: PATHS
+    },
+
+    entry: {
+        app: PATHS.src
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].js`,
+        path: PATHS.dist,
+        publicPath: ""
+    },
+    module: {
+        rules: [{
+            test: /\.pug$/,
+            oneOf: [{
+                resourceQuery: /^\?vue/,
+                use: ["pug-plain-loader"]
+            }, {
+                use: ['html-loader', 'pug-html-loader']
+            }]
+        }, {
+            test: /\.js$/,
+            loader: "babel-loader",
+            exclude: "/node_modules/"
+        }, {
+            test: /\.js|jsx$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [
+                        "@babel/plugin-transform-react-jsx",
+                        "@babel/plugin-proposal-class-properties"
+                    ]
+                }
+            }
+        }, {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file-loader",
+            options: {
+                name: "[name].[ext]"
+            }
+        }, {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: "file-loader",
+            options: {
+                name: "[name].[ext]"
+            }
+        }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {sourceMap: true}
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {sourceMap: true, config: {path: `${PATHS.src}/js/postcss.config.js}`}}
+                },
+                {
+                    loader: "sass-loader",
+                    options: {sourceMap: true}
+                }
+            ],
+        }, {
+            test: /\.css$/,
+            use: [
+                "style-loader",
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {sourceMap: true}
+                },
+                {
+                    loader: "postcss-loader",
+                    options: {sourceMap: true, config: {path: `${PATHS.src}/js/postcss.config.js}`}}
+                }
+            ]
+        }]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].css`,
+        }),
+        new CopyWebpackPlugin([
+            {from: `${PATHS.src}/images`, to: `${PATHS.assets}images`},
+            {from: `${PATHS.src}/fonts`, to: `${PATHS.assets}fonts`},
+            {from: `${PATHS.src}/static`, to: ""}
+
+        ]),
+        ...PAGES.map(page => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug/,'.html')}`
+        }))
+    ]
+}
---
+++ src/scripts/articles.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 123451,
+        title: "The First article",
+        text: "first article text first article text first article text  ",
+        date: "01.01.01"
+    },{
+        id: 123452,
+        title: "The Second article",
+        text: "second article text second article text second article text ",
+        date: "02.02.02"
+    },{
+        id: 123453,
+        title: "The Third article",
+        text: "third article text third article text third article text ",
+        date: "03.03.03"
+    },{
+        id: 123454,
+        title: "The Fourth article",
+        text: "fourth article text fourth article text fourth article text ",
+        date: "04.04.04"
+    }
+]
